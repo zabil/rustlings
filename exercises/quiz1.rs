ea7423f32d6135e3bd436e73aeaa9d20fe10a59a
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2022-03-24 11:54:21 +0000

    In progress

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,11 +8,13 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
-// Put your function here!
-// fn calculate_apple_price {
-
+fn calculate_apple_price(price: i8) -> i8 {
+    if price == 65 {
+        return price;
+    } else {
+        return price * 2;
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
